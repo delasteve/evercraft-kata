8262d239ebc0fdcd93e8a7bdc3edf9583a4a64be
Guy Royse <guy@guyroyse.com>
2014-09-11 21:29:10 -0400

    kata efforts for today

--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,35 +1,108 @@
-describe("Application", function() {
-
-  beforeEach(function() {
-    setFixtures('<div id="awesome"></div>');
-  });
-
-  when("made awesome", function() {
+describe("Evercraft", function() {
+  describe("Character", function() {
+    var subject, opponent;
 
     beforeEach(function() {
-      App.makeAwesome();
+      subject = Character.create();
+      opponent = Character.create();
     });
-
-    it("is awesome", function() {
-      expect(App.awesome()).toBe(true);
+    
+    it("has a name", function() {
+      subject.name('Bob');
+      expect(subject.name()).toBe('Bob');
     });
-
-    it("contains awesomeness", function() {
-      expect($('#awesome')).toContainText("Awesome App is Awesome");
+    
+    it('has a valid alignment', function () {
+      subject.alignment('Good');
+      expect(subject.alignment()).toBe('Good');
+      
+      subject.alignment('Neutral');
+      expect(subject.alignment()).toBe('Neutral');
+      
+      subject.alignment('Evil');
+      expect(subject.alignment()).toBe('Evil');
     });
+    
+    it('cannot have an invalid alignment', function () {
+      expect(function() {
+        subject.alignment('Jaded');
+      }).toThrow("Invalid alignment 'Jaded'");
+    });
+    
+    it('has a default hit points of 5', function () {
+      expect(subject.hitPoints()).toBe(5);
+    });
+    
+    it('has a default armor class of 10', function () {
+      expect(subject.armorClass()).toBe(10);
+    });
+    
+    when('attacking', function () {
+      
+      and('roll beats armor class', function () {
+        
+        var hit;
+        var expectedHitPoints;
+        
+        beforeEach(function () {
+          spyOn(opponent, 'armorClass').and.returnValue(5);
+          expectedHitPoints = opponent.hitPoints() - 1;
+          hit = subject.attack(opponent, 10);
+        });
 
+        it('hits opponent', function () {
+          expect(hit).toBe(true);
+        });
+        
+        it('deals a point of damage to the opponent', function () {
+          expect(opponent.hitPoints()).toBe(expectedHitPoints);
+        });
+        
+      });
+      
+      and('roll meets armor class', function () {
+        
+        var hit;
+        var expectedHitPoints;
+        
+        beforeEach(function () {
+          spyOn(opponent, 'armorClass').and.returnValue(10);
+          expectedHitPoints = opponent.hitPoints() - 1;
+          hit = subject.attack(opponent, 10);
+        });
+
+        it('hits opponent', function () {
+          expect(hit).toBe(true);
+        });
+        
+        it('deals a point of damage to the opponent', function () {
+          expect(opponent.hitPoints()).toBe(expectedHitPoints);
+        });
+        
+      });
+      
+      and('roll does not meet or beat armor class', function () {
+        
+        var hit;
+        var expectedHitPoints;
+        
+        beforeEach(function () {
+          spyOn(opponent, 'armorClass').and.returnValue(15);
+          expectedHitPoints = opponent.hitPoints();
+          hit = subject.attack(opponent, 10);
+        });
+
+        it('hits opponent', function () {
+          expect(hit).toBe(false);
+        });
+        
+        it('deals a point of damage to the opponent', function () {
+          expect(opponent.hitPoints()).toBe(expectedHitPoints);
+        });
+        
+      });
+      
+    });
+    
   });
-
-  when("not made awesome", function() {
-
-    it("is not awesome", function() {
-      expect(App.awesome()).toBe(false);
-    });
-
-    it("does not contain awesomeness", function() {
-      expect($('#awesome')).not.toContainText("Awesome App is Awesome");
-    });
-
-  });
-
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,53 @@
-var App = App || {};
+var Character = {
+  
+  create: function () {
+    var _fields = {};
 
-App.makeAwesome = function() {
-  $('#awesome').text("Awesome App is Awesome");
-};
+    function assignAndReturnFn(field) {
+      return function(passedInVar) {
+        if (passedInVar !== undefined) _fields[field] = passedInVar;
+        return _fields[field];
+      }
+    }
+    
+    function validAlignment (alignment) {
+      var validAlignments = ['Good', 'Neutral', 'Evil'];
+      return validAlignments.indexOf(alignment) > -1;
+    }
 
-App.awesome = function() {
-  return $('#awesome').text() === "Awesome App is Awesome";
+    return {
+      
+      name: assignAndReturnFn('name'),
+      
+      alignment: function (alignment) {
+        var assignAndReturn = assignAndReturnFn('alignment');
+        if (alignment === undefined) return assignAndReturn();
+        if (!validAlignment(alignment)) throw "Invalid alignment '" + alignment + "'";
+        return assignAndReturn(alignment);    
+      },
+      
+      hitPoints: function () {
+        var hitPoints = assignAndReturnFn('hitPoints');
+        if (hitPoints() === undefined) hitPoints(5);
+        return hitPoints();
+      },
+      
+      armorClass: function() {
+        return 10;
+      },
+      
+      attack: function (opponent, roll) {
+        if(roll >= opponent.armorClass()) {
+          opponent.damage();
+          return true;
+        }
+        
+        return false;
+      },
+      
+      damage: function () {
+        _fields.hitPoints--;
+      }
+    };
+  }
 };
